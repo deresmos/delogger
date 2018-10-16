d4a238597fd204d3ad4c74d0349493096530a43b
deresmos <deresmos@gmail.com>
2018-10-17 00:27:57 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+__author__ = 'deresmos'
+
+setup(
+    name='delogger',
+    version='0.0.1',
+    description='delogger is a convenient logging package',
+    author='deresmos',
+    author_email='deresmos@gmail.com',
+    packages=find_packages(),
+    include_package_data=False,
+    keywords=['logging', 'Logger'],
+    license='MIT License',
+    install_requires=['requests'],
+    extras_require={
+        'develop': [
+            'flake8',
+            'pytest',
+            'pytest-sugar',
+        ],
+    },
+)
