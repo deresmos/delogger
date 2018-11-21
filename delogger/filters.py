7bd6bdd63e1fe4f3b7cef714167d2dc4b0298a63
deresmos <deresmos@gmail.com>
2018-11-21 22:20:16 +0900

    Add docstring

--- delogger/filters.py
+++ delogger/filters.py
@@ -1,4 +1,14 @@
 class OnlyFilter(object):
+    """A filter that performs control to output only the specified level.
+
+    Args:
+        level (int): Handler level.
+
+    Attributes:
+        _level (int): Handler level.
+
+    """
+
     def __init__(self, level):
         self._level = level
 
