62be8c76964e5b7994c50a6102dd55db613bde87
deresmos <deresmos@gmail.com>
2018-12-09 23:05:43 +0900

    Update v0.1.4
    Update README

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages, setup
 
+with open('./README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name='delogger',
-    version='0.1.3',
+    version='0.1.4',
     description='delogger is a convenient logging package',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='deresmos',
     author_email='deresmos@gmail.com',
+    url='https://github.com/deresmos/delogger',
     python_requires='>=3.4',
     packages=find_packages(),
     include_package_data=False,
