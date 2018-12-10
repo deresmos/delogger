391ad02805ea0c3f9ebe19a1935ba15e7c7a191a
deresmos <deresmos@gmail.com>
2018-12-10 21:55:15 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='delogger',
-    version='0.1.4',
+    version='0.1.5',
     description='delogger is a convenient logging package',
     long_description=readme,
     long_description_content_type='text/markdown',
