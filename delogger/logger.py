fd0063ce73bc34c930c55e4dffb57033ecc5058e
deresmos <deresmos@gmail.com>
2018-12-13 23:50:02 +0900

    Add debuglog_line_memory_profiler
    line_profiler and memory_profiler mix output

--- delogger/logger.py
+++ delogger/logger.py
@@ -25,6 +25,10 @@ except ImportError:
     pass
 
 
+class LineEmpty(Exception):
+    pass
+
+
 class DeloggerSetting(object):
     """Common configuration class of Delogger.
 
@@ -212,6 +216,14 @@ class Delogger(DeloggerSetting):
     ERROR = ERROR
     CRITICAL = CRITICAL
 
+    L_M_HEADER_INDEX = 6
+    L_M_SEPARATOR_INDEX = 7
+    L_M_TEMPLATE = '{0:>6} {1:>9} {2:>12} {3:>8} {4:>8} {5:>12} {6:>12}   {7:<}'
+    L_M_HEADER = [
+        'Line #', 'Hits', 'Time', 'Per Hit', '% Time', 'Mem usage',
+        'Increment', 'Line Contents'
+    ]
+
     def __init__(self, name=None, parent=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -306,6 +318,105 @@ class Delogger(DeloggerSetting):
             return rtn
 
         return wrapper
+
+    @classmethod
+    def debuglog_line_memory_profiler(cls, func):
+        """line_profiler and memory_profiler mix are output to the log.
+        """
+
+        logger = cls(name='_debugger_l_m').logger
+
+        def wrapper(*args, **kwargs):
+            # memory_profiler
+            with StringIO() as f:
+                rtn = profile(func, stream=f, precision=2)(*args, **kwargs)
+                memory_value = cls._memory_profiler_parse(f.getvalue())
+
+            # line_profiler
+            prof = LineProfiler()
+            prof.add_function(func)
+
+            rtn = prof.runcall(func, *args, **kwargs)
+            with StringIO() as f:
+                prof.print_stats(stream=f)
+                mix, line_tmp = cls._line_profiler_parse(f.getvalue())
+
+            # memory line mix output
+            template = cls.L_M_TEMPLATE
+            for l, m in zip(line_tmp, memory_value):
+                l_m_mix = l[:5] + m
+                mix.append(template.format(*l_m_mix))
+            mix[cls.L_M_HEADER_INDEX] = template.format(*cls.L_M_HEADER)
+            mix[cls.L_M_SEPARATOR_INDEX] += '=' * 27
+            logger.debug('\n' + '\n'.join(mix))
+
+            return rtn
+
+        return wrapper
+
+    @staticmethod
+    def _memory_profiler_parse(result):
+        for line in result.split('\n'):
+            try:
+                elem = line.split()
+                if not elem:
+                    continue
+
+                lineno = elem[0].isnumeric()
+                if not lineno:
+                    continue
+
+                contents = line[35:]
+                if len(elem) < 2:
+                    raise LineEmpty()
+                float(elem[1])
+
+                mem = elem[1:3]
+                inc_mem = elem[3:5]
+                yield [' '.join(mem), ' '.join(inc_mem), contents]
+
+            except ValueError:
+                yield ['', '', contents]
+                continue
+            except LineEmpty:
+                yield ['', '', '']
+                continue
+
+    @staticmethod
+    def _line_profiler_parse(result):
+        mix = []
+        line_tmp = []
+        for line in result.split('\n'):
+            try:
+                elem = line.split()
+                if not elem:
+                    mix.append(line)
+                    continue
+
+                is_lineno = elem[0].isnumeric()
+                if not is_lineno:
+                    mix.append(line)
+                    continue
+
+                if len(elem) < 2:
+                    raise LineEmpty()
+                float(elem[1])
+                line_tmp.append(elem[:5])
+
+            except ValueError:
+                line_tmp.append([elem[0], '', '', '', ''])
+                continue
+            except LineEmpty:
+                line_tmp.append([elem[0], '', '', '', ''])
+                continue
+
+        for _ in range(5):
+            if not mix[-1]:
+                del mix[-1]
+            else:
+                break
+
+        return mix, line_tmp
 
     def default_logger(self):
         """Set default handler."""
