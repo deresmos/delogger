4ff7c9d19b6c2e00ec848c44f3e3f8f0b431a00a
deresmos <deresmos@gmail.com>
2019-10-06 09:58:19 +0900

    Add slack notify in debug_logger

--- delogger/loggers/debug_logger.py
+++ delogger/loggers/debug_logger.py
@@ -1,3 +1,5 @@
+import os
+
 from delogger import Delogger
 
 
@@ -6,6 +8,17 @@ def _set_logger():
     delogger.is_debug_stream = True
     delogger.is_color_stream = True
     delogger.is_save_file = True
+
+    slack_hdlr = None
+    if os.environ.get("DELOGGER_SLACK_URL", None):
+        slack_hdlr = SlackHandler()
+    elif os.environ.get("DELOGGER_SLACK_TOKEN", None):
+        token = os.environ.get("DELOGGER_SLACK_TOKEN", None)
+        channel = os.environ.get("DELOGGER_SLACK_CHANNEL")
+        slack_hdlr = SlackHandler(token=token, channel=channel)
+
+    if slack_hdlr:
+        delogger.add_handler(slack_hdlr, INFO)
 
     return delogger.logger
 
