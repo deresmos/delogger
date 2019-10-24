60f0046b332ba4529a8d913e8672f970fb2ab0ee
deresmos <deresmos@gmail.com>
2019-10-24 20:55:06 +0900

    Add RunRotatingMode filepath test

--- tests/modes/test_file.py
+++ tests/modes/test_file.py
@@ -28,3 +28,19 @@ class TestFileMode(DeloggerTestBase):
         Assert._bool(logfile.filepath.exists())
         logfile.filepath.unlink()
         logfile.dirpath.rmdir()
+
+    def test_run_rotation_filepath_mode(self, capsys):
+        run_rotation_mode = RunRotatingMode(filepath="log/test.log")
+        delogger = Delogger(
+            "file_run_rotationg_filepath_mode", modes=[run_rotation_mode]
+        )
+        logger = delogger.get_logger()
+
+        logfile = run_rotation_mode.logfile
+
+        self.execute_log(logger)
+        self.check_log_file(logfile.filepath)
+
+        Assert._bool(logfile.filepath.exists())
+        logfile.filepath.unlink()
+        logfile.dirpath.rmdir()
