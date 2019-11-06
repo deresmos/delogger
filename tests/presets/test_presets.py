8b9611e65e905ef2c09030e2e563d9feec0d1b44
deresmos <deresmos@gmail.com>
2019-11-06 23:10:10 +0900

    Add presets test

---
+++ tests/presets/test_presets.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from shutil import rmtree
+
+from delogger import Delogger
+from tests.lib.base import Assert, DeloggerTestBase
+
+
+class TestPresets(DeloggerTestBase):
+    LOG_DIR_LIST = ["log"]
+
+    def teardown_method(self):
+        for log_dir in self.LOG_DIR_LIST:
+            if not Path(log_dir).is_dir():
+                continue
+            rmtree(log_dir)
+
+    def test_debug_logger(self, capsys):
+        from delogger.presets.debug_logger import logger
+
+        self.execute_log(logger)
+
+        self.check_debug_stream_log(logger, capsys, is_color=True)
+
+        run_rotating_hdlr = logger.handlers[1]
+        self.check_log_file(run_rotating_hdlr.filepath)
+
+        assert getattr(logger, "debuglog")
