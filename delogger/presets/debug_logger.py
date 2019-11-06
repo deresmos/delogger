515d0cba96f9bb9eb2bce5f696157f3a80d8ea1f
deresmos <deresmos@gmail.com>
2019-11-06 22:52:13 +0900

    Add debug_log decorator in debug_logger

--- delogger/presets/debug_logger.py
+++ delogger/presets/debug_logger.py
@@ -1,4 +1,5 @@
 from delogger import Delogger
+from delogger.decorators.debug_log import DebugLog
 from delogger.modes.file import RunRotatingFileMode
 from delogger.modes.stream import ColorStremDebugMode
 
@@ -6,6 +7,7 @@ from delogger.modes.stream import ColorStremDebugMode
 def _get_logger():
     delogger = Delogger("debug_logger")
     delogger.load_modes(ColorStremDebugMode(), RunRotatingFileMode())
+    delogger.load_decorators(DebugLog())
 
     return delogger.get_logger()
 
