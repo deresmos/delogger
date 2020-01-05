b140aa0f11412dbe3b5bcedf82d8f9173f21db3b
deresmos <deresmos@gmail.com>
2020-01-05 21:00:48 +0900

    Add SlackWebhookMode

---
+++ delogger/modes/slack.py
@@ -0,0 +1,15 @@
+from logging import INFO
+from typing import Optional
+
+from delogger.handlers.slack import SlackHandler
+from delogger.modes.base import ModeBase
+
+
+class SlackWebhookMode(ModeBase):
+    def __init__(self, webhook_url: str, level: Optional[int] = INFO) -> None:
+        self.webhook_url = webhook_url
+        self.level = level
+
+    def load_to_delogger(self, delogger) -> None:
+        slack_hdlr = SlackHandler(url=self.webhook_url, as_user=True)
+        delogger.add_handler(slack_hdlr, self.level)
