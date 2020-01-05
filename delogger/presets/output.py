4fed91c435b5efcb28fdc6751694cd4695fb0551
deresmos <deresmos@gmail.com>
2020-01-05 21:01:02 +0900

    Gardening output presets

--- delogger/presets/output.py
+++ delogger/presets/output.py
@@ -1,9 +1,8 @@
 import os
-from logging import INFO, Logger
+from logging import Logger
 
 from delogger import DeloggerQueue
 from delogger.decorators.debug_log import DebugLog
-from delogger.handlers.slack import SlackHandler
 from delogger.modes.file import TimedRotatingFileMode
 
 
@@ -14,8 +13,9 @@ def _get_logger() -> Logger:
 
     slack_webhook = os.getenv("DELOGGER_SLACK_WEBHOOK")
     if slack_webhook:
-        slack_hdlr = SlackHandler(url=slack_webhook, as_user=True)
-        delogger.add_handler(slack_hdlr, INFO)
+        from delogger.modes.slack import SlackWebhookMode
+
+        delogger.load_modes(SlackWebhookMode(slack_webhook))
 
     return delogger.get_logger()
 
