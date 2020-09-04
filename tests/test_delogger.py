2c18e9828b7c3900fd11cb9e3d513fd5d1e3d118
deresmos <deresmos@gmail.com>
2020-09-04 18:16:36 +0900

    Fix test error

--- tests/test_delogger.py
+++ tests/test_delogger.py
@@ -1,27 +1,27 @@
 from tests.lib.base import DeloggerTestBase
 from delogger import Delogger
 from delogger.decorators.debug_log import DebugLog
-from delogger.modes.stream import StreamColorDebugMode
+from delogger.modes.stream import StreamDebugMode
 
 
 class TestDelogger(DeloggerTestBase):
     def test_delogger(self, capsys):
         delogger = Delogger("test_delogger")
-        delogger.load_modes(StreamColorDebugMode())
+        delogger.load_modes(StreamDebugMode())
         delogger.load_decorators(DebugLog())
 
         logger = delogger.get_logger()
 
         self.execute_log(logger)
 
-        self.check_debug_stream_log(logger, capsys, is_color=True)
+        self.check_debug_stream_log(logger, capsys, is_color=False)
 
         assert getattr(logger, "debuglog")
 
     def test_delogger_constructor(self, capsys):
         delogger = Delogger(
             name="test_delogger_c",
-            modes=[StreamColorDebugMode()],
+            modes=[StreamDebugMode()],
             decorators=[DebugLog()],
         )
 
@@ -29,6 +29,6 @@ class TestDelogger(DeloggerTestBase):
 
         self.execute_log(logger)
 
-        self.check_debug_stream_log(logger, capsys, is_color=True)
+        self.check_debug_stream_log(logger, capsys, is_color=False)
 
         assert getattr(logger, "debuglog")
